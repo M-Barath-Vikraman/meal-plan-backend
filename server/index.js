e83f472173a736f02981e57bd95f1328bdd7f5d8
M-Barath-Vikraman <barathvikraman008@gmail.com>
2026-09-16 14:30:08 +0530

    added amplify cors

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: ['http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://main.d3ovzn44hxkswc.amplifyapp.com'
+    'https://main.dog3dcvao5gjr.amplifyapp.com/',
   ],
   credentials: true,
 }));
